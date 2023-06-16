e29fdc89329629533c7c2963953ba327d68626bc
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-06-16 23:16:44 +0300

    Add files via upload

---
+++ beginner/src/components/Test.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            a:10
+        }
+        console.log("Constructor");
+    }
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.setState({
+            a: 20
+        })
+     }
+     componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+     } 
+     shouldComponentUpdate(){
+        console.log("shouldComponentUpdate");
+        return true;
+     }
+  render() {
+    console.log("Render");
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+export default Test;
---
+++ beginner/src/components/User.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import PropTypes from "prop-types"
+import UserConsumer from '../context'
+
+class User extends Component {
+
+  state = {
+    isVisible : false
+  }
+
+  /*constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible : true
+    }
+
+  }*/
+
+  /*constructor(props){
+    super(props);
+    this.onClickEvent = this.onClickEvent.bind(this);
+  }*/
+
+  onClickEvent = (e) =>{
+    this.setState({
+      isVisible: !this.state.isVisible
+    })
+  }
+
+  onDeleteUser = (dispatch, e) =>{
+    const {id} = this.props;
+    dispatch({type : "DELETE_USER", payload:id});
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+   }
+
+  render() {
+
+    const {name, department, salary} = this.props;
+    const {isVisible} = this.state;
+
+    return (
+      <UserConsumer>
+        {
+          value => {
+            const {dispatch} = value;
+            return (
+              <div className='col-md-8 mb-4'>
+                <div className='card' style = {isVisible ? {backgroundColor : "#62848d", color : "white"} : null}>
+                  <div className='card-header d-flex justify-content-between'>
+                    <h4 className='d-inline' onClick={this.onClickEvent.bind(this)}>{name}</h4>
+                    <i onClick = {this.onDeleteUser.bind(this, dispatch)} className='far fa-trash-alt' style = {{cursor : "pointer"}}></i>
+                  </div>
+        
+                  {
+                    isVisible ? <div className='card-body'>
+        
+                    <p className='card-text'>Maaş: {salary}</p>
+                    <p className='card-text'>Departman: {department}</p>
+                  </div> : null
+                  }
+                </div>
+                
+              </div>
+            )
+          }
+        }
+      </UserConsumer>
+    )
+  }
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  salary: PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
+}
+export default User;
